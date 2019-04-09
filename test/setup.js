005a5b49e43178e4043f5db6cc67a74f7a766e16
maxwellbenton <maxwellbenton@gmail.com>
2019-04-09 18:46:44 -0400

    moves helpers to setup.js

---
+++ test/setup.js
@@ -0,0 +1,32 @@
+const chai = require( 'chai' )
+global.expect = chai.expect
+const fs = require( 'file-system' )
+const jsdom = require( 'mocha-jsdom' )
+const path = require( 'path' )
+const babel = require( 'babel-core' );
+
+const html = fs.readFileSync( path.resolve( __dirname, '..', 'index.html' ), 'utf-8' )
+
+const babelResult = babel.transformFileSync(
+  path.resolve( __dirname, '..', 'index.js' ), {
+    presets: [ 'env' ]
+  }
+);
+
+const src = babelResult.code
+
+global.rot13 = ( str ) => {
+  var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  var index = x => input.indexOf( x );
+  var translate = x => index( x ) > -1 ? output[ index( x ) ] : x;
+  return str.split( '' )
+    .map( translate )
+    .join( '' );
+}
+
+jsdom( {
+  html,
+  src,
+  url: "http://localhost"
+} );
