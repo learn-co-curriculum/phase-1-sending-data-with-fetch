70828645fac03fa44dcb3fe6fe2f6ba53b5ec482
Maxwell Benton <maxwellbenton@gmail.com>
2019-04-09 19:39:53 -0400

    removes global.rot

--- test/setup.js
+++ test/setup.js
@@ -15,16 +15,6 @@ const babelResult = babel.transformFileSync(
 
 const src = babelResult.code
 
-global.rot13 = ( str ) => {
-  var input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  var output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-  var index = x => input.indexOf( x );
-  var translate = x => index( x ) > -1 ? output[ index( x ) ] : x;
-  return str.split( '' )
-    .map( translate )
-    .join( '' );
-}
-
 jsdom( {
   html,
   src,
